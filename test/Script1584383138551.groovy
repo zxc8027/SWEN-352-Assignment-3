731eff76ab93ddaaa4586affc5a7af55609accb3
Melissa Gould <melissa8g007@gmail.com>
2020-03-16 15:10:39 -0400

    Use Case 2 Script

---
+++ test/Script1584383138551.groovy
@@ -0,0 +1,32 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.amazon.com/')
+
+WebUI.setText(findTestObject('Page_Amazoncom Online Shopping for Electron_9e43a3/input_Search_field-keywords'), 'Javascript for dummies')
+
+WebUI.click(findTestObject('Object Repository/Page_Amazoncom Online Shopping for Electron_9e43a3/input_Go_nav-input'))
+
+WebUI.click(findTestObject('Page_Amazoncom  Javascript for dummies/span_Coding with JavaScript FD (For Dummies_46decd'))
+
+WebUI.click(findTestObject('Page_Coding with JavaScript FD (For Dummies_1190a4/input_Important_submitadd-to-cart'))
+
+WebUI.closeBrowser()
+
